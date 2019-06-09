66822c0c939e19863d6535d0753d083cf5dfa9de
Daniele Madama <danielemadama@danynb-3.home>
2019-06-09 15:30:17 +0200

    Fixes

--- src/audio-phone-booth.py
+++ src/audio-phone-booth.py
@@ -30,13 +30,13 @@ print("Waiting for keys...")
 track = ""
 
 def playAudio(audioFile):
-    if mixer.music.get_busy:
-        mixer.music.stop
+    if mixer.music.get_busy():
+        mixer.music.stop()
     print("Playing {}".format(audioFile))
     if not Path(audioFile).is_file():
         files = os.listdir(AUDIO_NOT_FOUND_PATH)
-        notFoundTrack = random.randrange(0, len(files))
-        playAudio(AUDIO_NOT_FOUND_PATH+notFoundTrack+AUDIO_EXTENSION)
+        notFoundTrack = files[random.randrange(0, len(files))]
+        playAudio(AUDIO_NOT_FOUND_PATH+notFoundTrack)
     mixer.music.load(audioFile)
     mixer.music.play()
 
@@ -49,8 +49,8 @@ def printKey(key):
         track = ""
     elif key=="*":
         print("Reset")
-        if mixer.music.get_busy:
-            mixer.music.stop
+        if mixer.music.get_busy():
+            mixer.music.stop()
         track = ""
     else:
         track += key

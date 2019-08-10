e0298a5b6d892075544b05f80056bd2da2453ae3
Daniele Madama <d.madama@gmail.com>
2019-08-10 13:40:02 +0200

    Improve with TTS for IP address

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 sleep 10
 amixer set PCM 100%
+
+# If networking is detected, the IP address is spelled
+WLAN_IP=`ifconfig wlan0 | grep 'inet ' | awk '{ print $2 }'`
+if [[ -n "$WLAN_IP" ]]; then
+    espeak "My IP address is $WLAN_IP" -s 100 --stdout | aplay
+fi
+
+# start python program
 cd /home/pi/audio-phone-booth/src
 python3 -u audio-phone-booth.py
